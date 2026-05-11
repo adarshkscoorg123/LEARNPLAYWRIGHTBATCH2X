d8a75f9aaed379af995ca7dd89340905cda27ebc
adarshkscoorg123 <adarsh_coorg123@yahoo.com>
2026-05-11 15:36:49 +0530

    Initial commit

---
+++ chapter_01_Basics/01_Basics.js
@@ -0,0 +1,4 @@
+console.log("Starting the first JavaScript Program");
+
+let x = 10;
+console.log(x);
---
+++ chapter_01_Basics/02_JS.js
@@ -0,0 +1,12 @@
+
+let a = 50;
+console.log(a);
+
+for(let b= 0; b < 10000; b++){
+    console.log(b);
+    print();
+}
+
+function print(){
+    console.log("Hello");
+}
